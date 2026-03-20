4ebc4437cc19c5dbcf18379bec031bbc07687548
Smith L Surendran <42495076+smithSurendran@users.noreply.github.com>
2026-03-19 23:45:00 -0600

    Temproray removed the website

--- src/App.js
+++ src/App.js
@@ -13,12 +13,16 @@ function App() {
       <Router>
         {/* <Navbar /> */}
         <Routes>
-          {/* <Route path="/" element={<Home />} />
+          <Route path="*" element={<div style={{padding: 40}}>We’ll be back soon.</div>} />
+        </Routes>
+
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/services" element={<Navigate to={`/services/${DEFAULT_SERVICE_SLUG}`} replace />} />
           <Route path="/services/:slug" element={<Services />} />
-          <Route path="/contact" element={<Contact />} /> */}
-        </Routes>
+          <Route path="/contact" element={<Contact />} />
+        </Routes> */}
       </Router>
     </div>
   );
